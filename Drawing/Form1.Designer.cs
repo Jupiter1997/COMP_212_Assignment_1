508f7709d69c2eac59eed518e76265f80db1b8fe
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2018-01-31 21:23:10 -0500

    Continue Drawing Panel

---
+++ Drawing/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GBcolor = new System.Windows.Forms.GroupBox();
+            this.radRed = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.GBsize = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.GBcolor.SuspendLayout();
+            this.GBsize.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(171, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 270);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // GBcolor
+            // 
+            this.GBcolor.Controls.Add(this.radioButton4);
+            this.GBcolor.Controls.Add(this.radioButton3);
+            this.GBcolor.Controls.Add(this.radBlue);
+            this.GBcolor.Controls.Add(this.radRed);
+            this.GBcolor.Location = new System.Drawing.Point(24, 12);
+            this.GBcolor.Name = "GBcolor";
+            this.GBcolor.Size = new System.Drawing.Size(123, 122);
+            this.GBcolor.TabIndex = 1;
+            this.GBcolor.TabStop = false;
+            this.GBcolor.Text = "Color";
+            // 
+            // radRed
+            // 
+            this.radRed.AutoSize = true;
+            this.radRed.Location = new System.Drawing.Point(18, 19);
+            this.radRed.Name = "radRed";
+            this.radRed.Size = new System.Drawing.Size(45, 17);
+            this.radRed.TabIndex = 0;
+            this.radRed.TabStop = true;
+            this.radRed.Text = "Red";
+            this.radRed.UseVisualStyleBackColor = true;
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.Location = new System.Drawing.Point(18, 42);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(46, 17);
+            this.radBlue.TabIndex = 1;
+            this.radBlue.TabStop = true;
+            this.radBlue.Text = "Blue";
+            this.radBlue.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(18, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(54, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Green";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(18, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(52, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // GBsize
+            // 
+            this.GBsize.Controls.Add(this.radioButton6);
+            this.GBsize.Controls.Add(this.radioButton7);
+            this.GBsize.Controls.Add(this.radioButton8);
+            this.GBsize.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.GBsize.Location = new System.Drawing.Point(24, 154);
+            this.GBsize.Name = "GBsize";
+            this.GBsize.Size = new System.Drawing.Size(123, 110);
+            this.GBsize.TabIndex = 4;
+            this.GBsize.TabStop = false;
+            this.GBsize.Text = "Size";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(18, 65);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(52, 17);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Large";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(18, 42);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(62, 17);
+            this.radioButton7.TabIndex = 1;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Medium";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(18, 19);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(50, 17);
+            this.radioButton8.TabIndex = 0;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Small";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 302);
+            this.Controls.Add(this.GBsize);
+            this.Controls.Add(this.GBcolor);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Drawing Panel";
+            this.GBcolor.ResumeLayout(false);
+            this.GBcolor.PerformLayout();
+            this.GBsize.ResumeLayout(false);
+            this.GBsize.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox GBcolor;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radBlue;
+        private System.Windows.Forms.RadioButton radRed;
+        private System.Windows.Forms.GroupBox GBsize;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+    }
+}
+
---
+++ Drawing/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        int y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+            x = p.X;
+            y = p.Y;
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+
+            if (radBlue.Checked)
+            {
+                SolidBrush sb = new SolidBrush(Color.Blue);
+                g.DrawLine(p, 50,50,100,100);
+                
+
+            }
+
+
+        }
+        //https://www.youtube.com/watch?v=R8KTm4E3gEA
+    }
+}
