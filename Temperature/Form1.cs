860248180e7b9802093d43c907bd8b480832a064
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2018-01-31 20:53:42 -0500

    Added Temperature Form

---
+++ Temperature/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // lblResult.Text = Convert.ToString((5/9) * (Convert.ToDouble(lblInput.Text) - 32)) + " °";
+            double input = Convert.ToDouble(lblInput.Text);
+            double result;
+
+            result = (5 * (input - 32)) / 9;
+
+            lblResult.Text = result.ToString("0.##");
+
+        }
+    }
+}
